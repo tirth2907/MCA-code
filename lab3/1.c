facae238770c53fcd1cef41ce967a411b9dd1e6d
tirth2907 <cryptorth2907@gmail.com>
2025-11-17 12:52:22 +0000

    3

---
+++ lab3/1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main() {
+    int A[2][2], B[2][2], C[2][2];
+    int i, j;
+
+    printf("Enter elements of 1st matrix:\n");
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 2; j++)
+            scanf("%d", &A[i][j]);
+
+    printf("Enter elements of 2nd matrix:\n");
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 2; j++)
+            scanf("%d", &B[i][j]);
+
+    // Matrix Addition
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 2; j++)
+            C[i][j] = A[i][j] + B[i][j];
+
+    printf("Resultant Matrix \n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++)
+            printf("%d ", C[i][j]);
+        printf("\n");
+    }
+}
---
+++ lab3/3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter size of square matrix (n): ");
+    scanf("%d", &n);
+
+    int A[n][n], T[n][n];
+
+    printf("Enter elements of %dx%d matrix:\n", n, n);
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &A[i][j]);
+
+    // Transpose
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            T[j][i] = A[i][j];
+
+    printf("Transpose of matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            printf("%d ", T[i][j]);
+        printf("\n");
+    }
+
+}
